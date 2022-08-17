ae442242fe2b6c97e61b5fc4ef41a771ecb1037a
Kade Fullmer <fullmer24@gmail.com>
2022-08-17 13:58:15 -0600

    House service brought in and updated

--- src/env.js
+++ src/env.js
@@ -2,6 +2,6 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 
-export const domain = 'classroomdemos.us.auth0.com'
+export const domain = 'mine24.us.auth0.com'
 export const audience = 'https://codeworksclassroom.com'
-export const clientId = '1RwYuMZc4P4BwvgujiBRqEybkumg1hww'
+export const clientId = 'OvisIjf9rdBOs2TNUn2uHIpLSdtHmulG'
---
+++ src/models/House.js
@@ -0,0 +1,28 @@
+export class House {
+    constructor({ id, style, bdrm, bath, price, imgUrl, description }) {
+        this.id = id
+        this.style = style || ''
+        this.bdrm = bdrm || ''
+        this.bath = bath || ''
+        this.price = price || 100000
+        this.img = imgUrl || ''
+        this.description = description || ''
+    }
+
+    get Template() {
+        return `
+    <div class="col-4 p-3">
+        <div class="bg-white elevation-2">
+            <img class="img-fluid" src="${this.img}" alt="">
+            <div class="p-2">
+                <h4 class="text-center">${this.style} | ${this.bdrm} | ${this.bath}</h4>
+                <p>${this.description}</p>
+                <p class="text-end text-success m-0">$<b>${this.price}</b></p>
+                <button class="btn btn-info" onclick="app.carsController.adjustCar('${this.id}')">Adjust Car Settings</button> 
+                <button class="btn btn-danger" onclick="app.carsController.deleteCar('${this.id}')">delete me</button> 
+            </div>
+        </div>
+    </div>
+    `
+    }
+}
